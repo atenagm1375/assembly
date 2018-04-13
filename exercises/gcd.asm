1c7620c896211277a287850d148e64e6f504f114
atena mohammadi <ashenatena@gmail.com>
2018-04-13 12:44:18 +0430

    add gcd exercise

---
+++ exercises/gcd.asm
@@ -0,0 +1,36 @@
+; exercise No.5, 08-Assembly3.pdf
+; write a program that computes GCD of two numbers{(AX, BX)->DX}.
+section .data
+    a dw 54
+    b dw 90
+
+section .text
+    global _start
+
+_start:
+    mov ax, [a]
+    mov bx, [b]
+    call GCD
+
+exit:
+    mov rax, 1
+    mov rbx, 0
+    int 80h
+
+GCD:
+    cmp ax, bx
+    jge while
+    xchg ax, bx
+    while:
+        xor dx, dx
+        div bx
+        cmp dx, 0
+        jne continue
+        mov dx, bx
+        jmp end
+        continue:
+        mov ax, bx
+        mov bx, dx
+        jmp while
+    end:
+    ret
