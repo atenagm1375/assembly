e8ad4d0942049ee7baea2978701e8a99898f2e45
atena mohammadi <ashenatena@gmail.com>
2018-05-04 12:26:58 +0430

    debug seri

--- exercises/seri.asm
+++ exercises/seri.asm
@@ -1,7 +1,7 @@
 ; exercise No.2, 12-Assembly6.pdf
 ; compute value of the following series: f(n, x)=sigma (x^k/k!) for k from 1 o n
 section .data
-    n dq 10
+    n dq 5
     x dq 5.e0
 
 section .text
@@ -18,26 +18,27 @@ exit:
     int 80h
 
 computeSigma:
-    enter 32, 0
-    mov rcx, [rbp+16]
+    enter 8, 0
+    mov rcx, [rbp+24]
     xor rax, rax
+    fldz
+    fld qword[rbp+16]
+    fld1
+    fld1
+    fldz
     while:
         inc rax
         cmp rax, rcx
         jg done
         mov [rbp-8], rax
         fild qword[rbp-8]
-        fmulp qword[rbp-16]
-        fld qword[rbp+24]
-        fmulp qword[rbp-24]
-        fldz
-        fstp qword[rbp-8]
-        fld qword[rbp-16]
-        faddp qword[rbp-8]
-        fld qword[rbp-24]
-        fmulp qword[rbp-8]
-        fld qword[rbp-8]
-        faddp qword [rbp-32]
+        fmulp st2
+        fld st2
+        fmul st4
+        fstp st3
+        fld st2
+        fdiv st2
+        faddp st1
         jmp while
     done:
     leave
