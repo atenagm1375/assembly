d8ab48b5c5e241708ec80ead2f1a4a9c4fefa039
atenagm <ashenatena@gmail.com>
2018-05-30 17:03:43 +0430

    give illegal input error, separate numbers and signs, TODOs are written as comment

--- proj2/calc.asm
+++ proj2/calc.asm
@@ -5,24 +5,27 @@
 section .data
     msg db "Enter a valid operation or x to exit:", 10
     msg_len equ $-msg
-    error db "Illegal operation. Please, try again.", 10
-    error_len equ $-error
+    errormsg db "Illegal operation. Please, try again.", 10
+    error_len equ $-errormsg
     divide_by_zero db "divide by zero", 10
     div_len equ $-divide_by_zero
     result dq 0
     max_num_size equ 19                 ; max digits of a number
-    max_inp_size equ 100                ; max size of input string
+    max_op_size equ 38
+    max_inp_size equ 200                ; max size of input string
 
 section .bss
     inp resb max_inp_size               ; keeps the input
     num resb max_num_size              ; keeps a number
-    ; op resb 1                           ; keeps operator
-    ; flag resb 1                         ; flags
+    op resb max_num_size                           ; keeps operator
+    flag resb 1                         ; flags
 
 section .text
     global _start
 
 _start:
+    xor bl, bl
+    mov byte[flag], bl
     mov rax, 1
     mov rdi, 0
     mov rsi, msg                        ; start message
@@ -31,8 +34,12 @@ _start:
 
     start_while:
     call readUserInput
-    cmp [rsi], byte 'x'
+    cmp [rsi], byte 'q'
     je exit
+    call evaluateInput
+    mov bl, 1
+    cmp byte[flag], bl
+    je _start
     jmp start_while
 
 exit:
@@ -54,4 +61,68 @@ readUserInput:
     mov rsi, inp
     mov rdx, max_inp_size
     syscall
+    ret
+
+error:
+    mov rax, 1
+    mov rdi, 0
+    mov rsi, errormsg
+    mov rdx, error_len
+    syscall
+    mov bl, 1
+    mov byte[flag], 1
+    ; TODO: add clear num and clear op
+    ret
+
+evaluateInput:
+    mov rsi, inp
+    mov rdi, num
+    mov rcx, max_inp_size
+    mov r14, op
+    evaluate_while:
+        mov r15b, byte[rsi]
+        cmp r15b, byte '0'
+        jl NAN
+        cmp r15b, byte '9'
+        jg NAN
+        ; TODO: convert string to integer
+        inc rsi
+        jmp evaluate_while
+
+    NAN:
+    cmp r15b, byte '='
+    je continue_eval
+
+    cmp r15b, byte '+'
+    je continue_eval
+
+    cmp r15b, byte '-'
+    je continue_eval
+
+    cmp r15b, byte '*'
+    je continue_eval
+
+    cmp r15b, byte '/'
+    je continue_eval
+
+    cmp r15b, byte '.'
+    je continue_eval
+
+    cmp r15b, byte '('
+    je continue_eval
+
+    cmp r15b, byte ')'
+    je continue_eval
+    call error
+    ret
+
+    continue_eval:
+    cmp r15b, byte '='
+    je end_eval
+    mov byte[r14], r15b
+    inc r14
+    inc rsi
+    jmp evaluate_while
+
+    end_eval:
     ret
