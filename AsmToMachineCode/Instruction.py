e6084bcdfe0575f270e9272b3c2858a153b9c613
atenagm <ashenatena@gmail.com>
2018-07-01 11:45:39 +0430

    analyze data for hexadecimal without alphabets

--- AsmToMachineCode/Instruction.py
+++ AsmToMachineCode/Instruction.py
@@ -11,6 +11,20 @@ class Instruction:
         self.arg_types = ['r', 'r']
         self.size = 0
 
+        self.address_prefix = ''
+        self.operand_prefix = ''
+        self.rex = ''
+        self.opcode = ''
+        self.mod = ''
+        self.reg = ''
+        self.rm = ''
+        self.data = ''
+        self.scale = ''
+        self.index = ''
+        self.base = ''
+        self.displacement = ''
+        self.extra = ''
+
     def validate_arguments(self):
         if (self.operation == 'not' or self.operation == 'mul') and self.args[1] is not None:
             raise InstructionError(invalid_num_of_args_msg)
@@ -49,7 +63,33 @@ class Instruction:
         ans = ''
         if self.arg_types[0] == 'm' and self.arg_types[1] == 'm':
             raise InstructionError(both_mem_msg)
+        self.opcode = opcode[self.operation][self.arg_types[0] + self.arg_types[1]]
+        if self.mode == size[3]:
+            self.rex = Rex
+        if self.arg_types[1] == 'd':
+            self.analyze_data(self.args[1])
         return ans
+
+    def analyze_data(self, val):
+        if self.mode == size[2]:
+            d = ['0'] * 32
+        else:
+            d = ['0'] * 40
+            self.extra = '0111' + '{0:24b}'.format(0)
+        if val[:2] == '0x':
+            val = val.replace('0x', '')
+            print(val)
+            if len(val) % 2 != 0:
+                val = '0' + val
+                print(val)
+            # d = list(self.data)
+            for i in range(len(val) - 1, -1, -2):
+                d[len(val) - i - 1] = '{0:04b}'.format(int(val[i - 1]))
+                d[len(val) - i] = '{0:04b}'.format(int(val[i]))
+            self.data = ''.join(d)
+            print(self.data)
+        else:
+            bin(int(val))
 
 
 class InstructionError(Exception):
