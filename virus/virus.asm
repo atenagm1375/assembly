14b389ce54fbb9ec388778886ae3c9660cb1616e
atenagm <ashenatena@gmail.com>
2018-07-17 17:18:24 +0430

    initial commit

--- virus/virus.asm
+++ virus/virus.asm
@@ -1,14 +1,11 @@
-global _start
-_start:     push    byte 4
-            pop     rax
-            xor     rbx, rbx
-            inc     rbx
-            mov     rcx, 0x0000000000400001
-            push    byte 3
-            pop     rdx
-            int     0x80
+section .text:
+    global virus_start
 
-            xor     rax, rax
-            inc     rax
-            xor     rbx, rbx
-            int     0x80
+virus_start:
+
+
+virus_exit:
+    xor     rax, rax
+    inc     rax
+    xor     rbx, rbx
+    int     0x80
